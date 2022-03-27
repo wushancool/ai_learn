7cfbaa9f1084d683f5e5af042572467de18b9563
WuShan <dave.wu.mail@gmail.com>
2022-03-27 11:37:45 +0800

    Add batch iou

--- cv/metrics/iou.py
+++ cv/metrics/iou.py
@@ -1,6 +1,10 @@
 import numpy as np
+import torch
+from functools import singledispatch
+from typing import List
 
-def iou(box1, box2, wh=False):
+@singledispatch
+def iou(box1:List[int], box2:List[int], wh=False):
     """
     Intersection over Union (IoU) for object detection.
     IoU = Area of Intersection of two boxes / Area of Union of two boxes.
@@ -29,4 +33,23 @@ def iou(box1, box2, wh=False):
     area_union = area(b1) + area(b2) - area_in
 
     return area_in / area_union
-    
+
+@iou.register
+def _(boxes1: torch.Tensor, boxes2: torch.Tensor):
+    """ Batch iou scores with generation and ground-truth boxes.
+    :param boxes1: (B1, 4)
+    :param boxes2: (B2, 4)
+    :return: (B1, B2), all iou scores between every boxes1 and boxes2.
+    """
+    # Intersections between every boxes1 with boxes2
+    left_top = torch.max(boxes1[:, None, :2], boxes2[:, :2]) # Broadcasting match every boxes2 to boxes1
+    right_bottom = torch.min(boxes1[:, None, 2:], boxes2[:, 2:])
+    h_w = (right_bottom - left_top).clamp(min = 0) # When height or width less than 0 means no intersection
+    inter_areas = h_w[:,:,0] * h_w[:,:,1]
+
+    # Union area
+    box_area = lambda b:(b[:,2] - b[:,0]) * (b[:,3] - b[:,1])
+    area1, area2 = box_area(boxes1), box_area(boxes2)
+    union_area = area1[:,None] + area2 # Broadcasting
+
+    return inter_areas / union_area
